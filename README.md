d8d2e2182a8ff241b1d991e410989e6a24277649
evanrittner <evanrittner@gmail.com>
2020-11-30 19:15:39 -0600

    Create files

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # find-and-replace
- Regex-based find and replace tool for editing subreddit wiki pages
+ Regex-based find and replace tool for editing subreddit wiki pages. Written by @evanrittner.
+ 
+Python 3. Requires the "praw" module (from the command line: "pip install praw").
+
+## Usage:
+
+The program is text-based, and hopefully pretty straightforward, but this guide is in case it isn't. 
+
+You'll first be directed to authorize the app to read and edit wiki pages on your behalf, which will open a browser window. This is so the edits that you make with the program will actually be made by your account. 
+
+For the word or phrase to search, you can enter some literal string, in plain english, or you can enter a regular expression. (Be warned, valid regex syntax will be interpreted as such; backslashes, for example, must be escaped.)
+
+To do more advanced replacements, where the replacement string depends on what exactly the found string is, you should be familiar with the Python re module. The replacement can be a function, which will take each match as an input, and return the replacement string. For an example, see the section below.
+
+For each match, the program will allow you to decide whether or not to make the replacement. You can, however, allow the program to make changes without confirmations.
+
+Finally, there is an adjustable "view margin". This is the number of characters to show you for each match, on either side of the match itself (to help you judge if each instance should be replaced or not). The larger this is, the clearer it is for you, but too large of a margin, and matches might overlap, in which case you would need to run the program again to replace all of them. It's set at a default of six characters on each side.
+
+The replacements you make are applied to reddit once you finish each page (not after each individual edit).
+
+
+## Example Execution
+This example showcases the regex and advanced replacement functionality. This converts links of the form "/r/SpaceX/<id>/" to "/r/SpaceX/comments/<id>/"). Case is ignored, to match both "/r/SpaceX" and "/r/spacex".
+`-=-=-=-=-=-=-=-=-=-=-
+  find-and-replace
+   /u/thatnerdguy1
+-=-=-=-=-=-=-=-=-=-=-
+
+Complete the authorization that opened in your browser.
+Authorization successful.
+(you can close the browser tab that opened)
+
+Enter the name of the subreddit: SpaceX
+Enter the pages to be edited, separated by spaces.
+To list all pages, enter "pages".
+To edit all pages, enter "all".
+all
+Enter the string to search (regex syntax is valid!): (r\/spacex\/)([a-zA-Z0-9]{6}[\/)])
+Ignore case? ("y"/"n"): y
+Enter the string to replace each instance with. For advanced functionality, enter "!ADV!": !ADV!
+Complete the lambda function, which takes a single match object as input, and outputs the replacement string: (or, enter "X" to return to the simple functionality)
+lambda match: match.group(1) + "comments/" + match.group(2)
+The current view margin is 6. Do you want to adjust it? ("y"/"n"): n
+Would you like to skip confirmation of each replacement? This is risky. ("y"/"n"): n
+
+Each match will be shown. Enter "y" to replace, or anything else to skip replacement.
+`
---
+++ find-and-replace.py
@@ -0,0 +1,123 @@
+import praw
+import re
+import os
+import random
+import socket
+import sys
+
+"""
+#Example: Convert links like "/r/Spacex/ikfxn6/" to "/r/SpaceX/comments/ikfxn6/"
+search: (r\/spacex\/)([a-zA-Z0-9]{6}[\/)])
+replace (adv.):  match.group(1) + "comments/" + match.group(2)
+"""
+
+def strip(page):
+    return str(page).split("/",1)[1]
+
+
+def replace(text, string, replacement, margin=5, ignorecase=False, risky=False):
+    changes_made = 0
+    matches = re.finditer(".{0," + str(margin) + "}" + string + ".{0," + str(margin) + "}", text, flags=(re.I if ignorecase else 0))
+    for m in matches:
+        print(m.group())
+        if risky or input().lower() == "y":
+            changes_made += 1
+            text = text[:m.start()] + re.sub(string, replacement, text[m.start():], count=1, flags=(re.I if ignorecase else 0))
+    return text, changes_made
+
+
+if __name__=="__main__":
+    #Header
+    print("-=-=-=-=-=-=-=-=-=-=-")
+    print("  find-and-replace")
+    print("   /u/thatnerdguy1")
+    print("-=-=-=-=-=-=-=-=-=-=-\n")
+    
+    
+    #Authorize
+    reddit = praw.Reddit(user_agent="find-and-replace by thatnerdguy1",
+                         client_id="LCLvKNitX8kktQ", client_secret=None,
+                         redirect_uri="http://localhost:8080")
+    state = str(random.randint(0, 65000))
+    print("Complete the authorization that opened in your browser.")
+    os.startfile(reddit.auth.url(["wikiread","wikiedit"], state, "temporary"))
+    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("localhost", 8080))
+    server.listen(1)
+    client = server.accept()[0]
+    server.close()
+    
+    data = client.recv(1024).decode("utf-8")
+    param_tokens = data.split(" ", 2)[1].split("?", 1)[1].split("&")
+    params = {key: value for (key, value) in [token.split("=") for token in param_tokens]}
+
+    if state != params["state"]:
+        raise Exception(f"State mismatch while processing authorization. Expected: {state} Received: {params['state']}")
+    elif "error" in params:
+        raise Exception("Error when processing authorization: " + params["error"])
+
+    refresh_token = reddit.auth.authorize(params["code"])
+    print("Authorization successful.\n(you can close the browser tab that opened)\n")
+    
+    
+    #Input
+    subreddit = input("Enter the name of the subreddit: ").split("/")[-1]
+    
+    print("Enter the pages to be edited, separated by spaces.")
+    print("To list all pages, enter \"pages\".")
+    print("To edit all pages, enter \"all\".")
+    
+    pages = input().split()
+    while (not pages) or ("pages" in pages): 
+        if "pages" in pages:
+            print(f"--- All pages in /r/{subreddit}: ---")
+            for p in reddit.subreddit(subreddit).wiki:
+                print(strip(p))
+        pages = input("Enter the pages to be edited. ").split()
+    if "all" in pages:
+        pages = reddit.subreddit(subreddit).wiki
+    else:
+        pages = map(lambda x: reddit.subreddit.wiki[x], pages)
+        
+    search = input("Enter the string to search (regex syntax is valid!): ")
+    
+    ignorecase = input("Ignore case? (\"y\"/\"n\"): ") == "y"
+    
+    while True:
+        new = input("Enter the string to replace each instance with. For advanced functionality, enter \"!ADV!\": ")
+        if new == "!ADV!":
+             func_in = input("Complete the lambda function, which takes a single match object as input, and outputs the replacement string: (or, enter \"X\" to return to the simple functionality)\nlambda match: ")
+             if func_in != "X":
+                 exec("new = lambda match: " + func_in)
+                 break
+        else:
+            break
+        
+    if input("The current view margin is 6. Do you want to adjust it? (\"y\"/\"n\"): ") == "y":
+        margin = int(input("Enter a new view margin: "))
+    else:
+        margin = 6
+        
+    risky = input("Would you like to skip confirmation of each replacement? This is risky. (\"y\"/\"n\"): ") == "y"
+    
+    
+    #Main loop through pages
+    total = 0
+    print("\nEach match will be shown. Enter \"y\" to replace, or anything else to skip replacement.")
+    for page in pages:
+        if not page.may_revise:
+            print(f"\nYou do not have permission to edit {strip(page)}. Proceeding.")
+            continue
+        print(f"\nPage {strip(page)}:\n")
+        
+        new_text, changes_made = replace(page.content_md, search, new, margin, ignorecase, risky)
+        
+        if changes_made:
+            if input(f"\nAll matches checked.\nConfirm {changes_made} change{'s' if total != 1 else ''} for {strip(page)}? (\"y\"/\"n\"): ") == "y":
+                page.edit(new_text, f"find and replace {search} with {new if isinstance(new,str) else '<function>'}")
+        total += changes_made
+        print(f"Finished page {strip(page)}.")
+        
+    print(f"\nAll selected pages complete. {total} total change{'s' if total != 1 else ''} made.")
